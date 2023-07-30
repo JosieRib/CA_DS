164cff4e1908fdfc1e118e957884f43bcaaeb293
JosieRib <129381451+JosieRib@users.noreply.github.com>
2023-07-30 12:29:44 +0100

    netstat -ano | findstr :50051

---
+++ HomeParking/src/test/java/org/example/client/BookingClientTest.java
@@ -0,0 +1,14 @@
+package org.example.client;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BookingClientTest {
+    @BeforeAll
+    public void setup(){
+
+
+
+    }
+}
